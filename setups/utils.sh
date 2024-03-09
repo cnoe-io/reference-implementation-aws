e390a3df7abc20c6549f2bd835493db2fde87643
Yatin03 <72162406+Yatin03@users.noreply.github.com>
2024-03-09 09:11:51 +0000

    Removed KUBECONFIG environment logic as terraform providers are reffing to /home/codespace/.kube/config file
    Signed-off-by: Yatin03 <72162406+Yatin03@users.noreply.github.com>

--- setups/utils.sh
+++ setups/utils.sh
@@ -19,31 +19,17 @@ for cli in "${clis[@]}"; do
   fi
 done
 
+DEFAULT_KUBECONFIG_FILE="$HOME/.kube/config"
+# Check if the default kubeconfig file exists
+if [ ! -f "${DEFAULT_KUBECONFIG_FILE}" ]; then
+    echo "${DEFAULT_KUBECONFIG_FILE} kubeconfig file does not exist. Exiting..."
+    exit 1
+fi
 
-# Check if KUBECONFIG environment variable is set
-if [ -z "$KUBECONFIG" ]; then
-    DEFAULT_KUBECONFIG_FILE="$HOME/.kube/config"
-    echo "KUBECONFIG variable is not set. Checking $DEFAULT_KUBECONFIG_FILE kubeconfig file."
-
-    # Check if the default kubeconfig file exists
-    if [ ! -f "${DEFAULT_KUBECONFIG_FILE}" ]; then
-        echo "${DEFAULT_KUBECONFIG_FILE} kubeconfig file does not exist and KUBECONFIG environment variable is not set correctly."
-        exit 1
-    fi
-
-    if [ "$( grep  -v "^$\|^ *$" -c  "${DEFAULT_KUBECONFIG_FILE}" )" -eq "0" ]; then
-        echo -e "${RED}Error: ${DEFAULT_KUBECONFIG_FILE} kubeconfig file does not exist or is empty.${NC}"
-        echo -e "${PURPLE}Info: Please configure a valid kubeconfig file or set the KUBECONFIG environment variable.${NC}"
-        exit 1
-    fi
-
-else
-    # Check if the kubeconfig file specified in the environment variable exists
-    if [ ! -f "$KUBECONFIG" ]; then
-        echo -e "${RED}Specified kubeconfig file '${KUBECONFIG}' does not exist.${NC}"
-        exit 1
-    fi
-    export KUBECONFIG
+if [ "$( grep  -v "^$\|^ *$" -c  "${DEFAULT_KUBECONFIG_FILE}" )" -eq "0" ]; then
+    echo -e "${RED}Error: ${DEFAULT_KUBECONFIG_FILE} kubeconfig file does not exist or is empty.${NC}"
+    echo -e "${PURPLE}Info: Please configure a valid kubeconfig file or set the KUBECONFIG environment variable.${NC}"
+    exit 1
 fi
 
 kubectl cluster-info > /dev/null
