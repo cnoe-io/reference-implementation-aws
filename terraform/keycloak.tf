3f0881f1e91108ce6d5fc4f7a6ddc0a2e4d6e669
Manabu McCloskey <manabu.mccloskey@gmail.com>
2024-03-19 12:09:36 -0700

    fix kc region var
    Signed-off-by: Manabu McCloskey <manabu.mccloskey@gmail.com>

--- terraform/keycloak.tf
+++ terraform/keycloak.tf
@@ -20,7 +20,7 @@ resource "aws_iam_policy" "external-secrets" {
           "secretsmanager:ListSecretVersionIds"
         ],
         "Resource": [
-          "arn:aws:secretsmanager:us-west-2:${data.aws_caller_identity.current.account_id}:secret:cnoe/keycloak/*"
+          "arn:aws:secretsmanager:${var.region}:${data.aws_caller_identity.current.account_id}:secret:cnoe/keycloak/*"
         ]
       }
     ]
